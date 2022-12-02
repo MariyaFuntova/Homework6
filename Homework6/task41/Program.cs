b72bbe16218aeab727484d9c7e89ea70e22ed955
Mariya <m_relaxeena@mail.ru>
2022-12-02 18:03:09 +0400

    gitignore

---
+++ Homework6/task41/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число M: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+int numbersLargerZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        } 
+    } 
+    return count;
+}
+
+int NumbersLargerZero = numbersLargerZero(array);
+Console.WriteLine($"Количество чисел больше 0 = {NumbersLargerZero}");
